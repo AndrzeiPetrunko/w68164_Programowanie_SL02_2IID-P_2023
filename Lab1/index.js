ce791796a66e03af68fe9f89c9428cf67187c4b8
Andrii <andriypetrnko@gmail.com>
2023-03-24 13:08:34 +0100

    as

--- Lab1/index.js
+++ Lab1/index.js
@@ -10,3 +10,7 @@ var a, b;
 a = prompt("Liczba 1")
 b = prompt("Liczba 2")
 console.log(Number(a) + Number(b))
+
+var date = new Date();
+	var current_time = date.getHours()+":"+date.getMinutes()+":"+ date.getSeconds();
+	document.getElementById("p1").innerHTML = current_time;
