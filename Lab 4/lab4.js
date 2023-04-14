c1cdefd0a7507264c1235760b75666e264e9050f
Andrii <andriypetrnko@gmail.com>
2023-04-14 22:38:41 +0200

    lab4

---
+++ Lab 4/lab4.js
@@ -0,0 +1,12 @@
+// Lab 4 - Zad 1
+function Ksiazka(tytul, autor, rok) {
+    this.tyt = tytul;
+    this.aut = autor;
+    this.rok = rok;
+    this.fullNazwa = function(){
+        return this.tyt + ' - ' + this.aut + ' in ' + this.rok
+}
+}
+    
+const k1 = new Ksiazka("Harry Potter", "J.K. Rowling", "20XX")
+console.log(k1.fullNazwa())
--- Lab1/index.js
+++ Lab1/index.js
@@ -1,58 +1,5 @@
-for (i = 0; i <= 100; i++) {
-    if (i % 2 == 1) {
-        continue
-
-    }
-    console.log(i + ", ")
-}
-
-var a, b;
-a = prompt("Liczba 1")
-b = prompt("Liczba 2")
-console.log(Number(a) + Number(b))
-
-function printTime() {
-    var d = new Date();
-    var hours = d.getHours();
-    var mins = d.getMinutes();
-    var secs = d.getSeconds();
-    document.getElementById("p1").innerHTML = hours+":"+mins+":"+secs;
-}
-setInterval(printTime, 1000);
 
 
-// Суть проги: у текстовому вікні ми даємо число, кнопкою запускаємо код. Він генерує число від 1 до 100. Кожен раз прога дає результат
-// наше число більше/менше ніж число проги. Якщо вони однакові, то прога зупиняється, дає нам кількість спроб, за які вона знайшла нашу
-// цифру і перезапускає сторінку. 
-
-
-
-function game() {
-    var number = document.getElementById("number").value
-    let attempts = 0
-    do {
-        var rand_num = Math.round(100 * Math.random());
-        attempts++;
-    }
-    while (number != rand_num)
-    document.write(`Our number is found within ${attempts} attempts !!!`)
-}
-//прив'язазати код на кнопку!!!!
-// element.removeEventListener("click", game); --- відв'язати від кнопки натискання
-
-
-// Lab 4 - Zad 1
-function Ksiazka(tytul, autor, rok) {
-    this.tyt = tytul;
-    this.aut = autor;
-    this.rok = rok;
-    this.fullNazwa = function(){
-        return this.tyt + ' - ' + this.aut + ' in ' + this.rok
-}
-}
-    
-const k1 = new Ksiazka("Harry Potter", "J.K. Rowling", "20XX")
-console.log(k1.fullNazwa())
 
 
 // Lab 4 - Zad 2
@@ -79,6 +26,6 @@ function Car(marka, model, rok_p, kolor, speed) {
     this.marka = marka
     this
 }
-// lmao
+
 
 
