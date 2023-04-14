2c162c34c1b88312a5fd8cededc3c8b79af1f7b7
Andrii <andriypetrnko@gmail.com>
2023-04-14 22:47:42 +0200

    lab4 zad 1 & 2

--- Lab 4/lab4.js
+++ Lab 4/lab4.js
@@ -4,9 +4,26 @@ function Ksiazka(tytul, autor, rok) {
     this.aut = autor;
     this.rok = rok;
     this.fullNazwa = function(){
-        return this.tyt + ' - ' + this.aut +  `(${this.rok})`
+        return this.tyt + ' - ' + this.aut +  ` (${this.rok})`
 }
 }
     
 const k1 = new Ksiazka("Harry Potter", "J.K. Rowling", "20XX")
 console.log(k1.fullNazwa())
+// Lab 4 - Zad 2
+function Student(imie, nazwisko, num_alb, o1, o2, o3) {
+    this.name = imie;
+    this.surname = nazwisko;
+    this.num_alb = num_alb;
+    this.grade1 = o1;
+    this.grade2 = o2;
+    this.grade3 = o3;
+    this.fullInfo = function () {
+        this.average = function() {
+            return Math.round((o1 + o2 + o3) / 3)
+        }
+        return this.name + ' ' + this.surname + ', ' + this.num_alb + ', ' + ' średnia ocena: ' + this.average()
+    }
+    }
+    const stud1 = new Student("John", "Smith", "w12345", 3, 5, 5)
+    console.log(stud1.fullInfo())
