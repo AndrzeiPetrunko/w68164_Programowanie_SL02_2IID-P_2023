5a3150234dc468ddf7a4d71a23c41376d6c7e077
Andrii <andriypetrnko@gmail.com>
2023-04-14 22:30:08 +0200

    zadanie5

--- Lab 3/lab3.js
+++ Lab 3/lab3.js
@@ -26,7 +26,7 @@ function Zadanie4() {
     
 }
 setInterval(Zadanie4, 1000)
-let tries = 0
+let tries = 1
 function Game() {
     let num2 = Math.round(Math.random() * 10)
     let num1 = Number(document.getElementById("t1").value)
