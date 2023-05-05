1cba956aff845b0b57c41f182dc7ff36d0084f3d
Andrii <andriypetrnko@gmail.com>
2023-05-05 12:54:04 +0200

    nie rozumiem, jak zrobić 2 i 3 zadanie z lab 6 :(

--- Lab 6/lab6.js
+++ Lab 6/lab6.js
@@ -1,5 +1,5 @@
 // Lab 6 - Zad 1
-let n = prompt("Podaj liczbę dla Zadania 1: ")
+/*let n = prompt("Podaj liczbę dla Zadania 1: ")
 function silnia(n) {
     if (n == 0 || n == 1 ) {
         return 1
@@ -11,5 +11,20 @@ function silnia(n) {
         while (n > 0)
     }
 }
-console.log(`Silnia ${n} jest równa ${silnia(n)}`)
+console.log(`Silnia ${n} jest równa ${silnia(n)}`) */
 // Lab 6 - Zad 2
+// Lab 6 - Zad 4
+const btn = document.getElementById('123')
+btn.addEventListener('click', clickButton)
+var txt = document.getElementById('txt1')
+clickIs = false
+const clickButton = function(){
+        if (clickIs == false) {
+            txt.textContent = "Hejka"
+            clickIs = true
+        }
+        else {
+            txt.textContent = " "
+            clickIs = false
+        }
+}
