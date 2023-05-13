96a27bb2472dcf2c6f226a39c350f8e7b7af3be7
Andrii <andriypetrnko@gmail.com>
2023-05-13 16:38:58 +0200

    lab6

--- Lab 6/lab6.js
+++ Lab 6/lab6.js
@@ -31,6 +31,36 @@ if(selected){
     selected = btn
 }
 // Lab 6 - Zad 3
-const btn4 = document.getElementById("i4")
-btn4.addEventListener('click', transparencyChange())
+  let button = document.querySelector('#i4')
+  button.addEventListener('click', function(){
+    let number = Math.floor(Math.random()*3 + 1 );
+    let element = document.querySelector('#el'+number);
+    if(element.style.visibility == ''){
+        element.style.visibility = 'hidden'
+    }
+    else{
+        element.style.visibility = '';  
+    }
+
+  })
 // Lab 6 - Zad 4
+let bttn = document.getElementById("123")
+let lista = document.getElementById("lista")
+bttn.addEventListener('click',()=>{
+let li = document.createElement('li')
+li.textContent = document.getElementById('txt1').value
+lista.appendChild(li)
+})
+// Lab 6 - Zad 5
+let btn5 = document.getElementById('btn5')
+let table = document.getElementById('table')
+btn5.addEventListener('click', ()=>{
+let tr = document.createElement('tr')
+let td1 = document.createElement("td")
+let td2 = document.createElement("td")
+td1.textContent = document.getElementById('txt1').value
+td2.textContent = document.getElementById('txt2').value
+tr.appendChild(td1)
+tr.appendChild(td2)
+table.appendChild(tr)
+})
