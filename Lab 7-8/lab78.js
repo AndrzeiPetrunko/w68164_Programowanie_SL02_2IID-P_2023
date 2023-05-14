10f111c51d020ee1a5f1145ab19fbb0e28546d36
Andrii <andriypetrnko@gmail.com>
2023-05-14 21:58:26 +0200

    12312

--- Lab 7-8/lab78.js
+++ Lab 7-8/lab78.js
@@ -1,12 +1,27 @@
 let imie = document.getElementById("imie")
 imie.addEventListener('input', () => {
  const imieReg = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
- if (!imieReg.test(imie.value)){
-    imie.setCustomValidity("halo?")
- }
+ if (!imieReg.test(field.value)){
+    field.setCustomValidity("asdsasd")
+}
 
  }
 );
 
+
+class samochod{
+    constructor(a, speed){
+        this.imie = a;
+        this.speed = speed;
+    }
+    speedChange(speed) {
+        this.speed += 10
+    }
+}
+
+let car = new samochod("BMW", speed)
+console.log(car)
+car.speedChange(speed)
+console.log(car)
 
 
