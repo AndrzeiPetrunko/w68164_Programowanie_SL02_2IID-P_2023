fb6fce390af811cecded8c426e677ba62801e448
Andrii <andriypetrnko@gmail.com>
2023-05-19 13:11:29 +0200

    kolokwium

--- Lab 7-8/lab78.js
+++ Lab 7-8/lab78.js
@@ -1,15 +1,22 @@
-let imie = document.getElementById("imie")
-imie.addEventListener('input', () => {
- const imieReg = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
- if (!imieReg.test(field.value)){
-    field.setCustomValidity("asdsasd")
-}
-
- }
-);
 
 
-class samochod{
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*class samochod{
     constructor(a, speed){
         this.imie = a;
         this.speed = speed;
@@ -29,5 +36,28 @@ let car = new samochod("BMW", 12)
 console.log(car)
 car.speedChange()
 console.log(car)
+console.log()
 
 
+
+class Osoba{
+    constructor(name, surname, age) {
+        this.name = name
+        this.surname = surname
+        this.age = age
+
+    }
+    setAge = (age) => {
+        this.age = age
+    }
+    fullName = () => {
+        return this.name + ' ' + this.surname
+    }
+}
+
+const Milosz = new Osoba('Miłosz', 'Dubiel', 21)
+console.log(Milosz)
+console.log(Milosz.setAge(30))
+console.log(Milosz.fullName)
+
+*/
